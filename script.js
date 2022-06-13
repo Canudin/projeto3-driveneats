d35f037db9662a126d9c64e3bf80bfd56a99f8a5
Giovanni Marcatto <gzmarcatto@gmail.com>
2022-06-13 17:51:04 -0300

    versão acabou o tempo

--- script.js
+++ script.js
@@ -1,44 +1,69 @@
 function highlight(choiceBox) {
-    const selecionado = document.querySelector(".highlight");
-    console.log(selecionado);
-    if (selecionado !== null) {
-        selecionado.classList.remove("highlight");
-    }
+  const selected = document.querySelector(".highlight");
+  if (selected !== null) {
+    selected.classList.remove("highlight");
+  }
   choiceBox.classList.toggle("highlight");
   purchase();
 }
 
 function highlight2(choiceBox) {
-    const selecionado = document.querySelector(".highlight2");
-    if (selecionado !== null) {
-        selecionado.classList.remove("highlight2");
-    }
+  const selected = document.querySelector(".highlight2");
+  if (selected !== null) {
+    selected.classList.remove("highlight2");
+  }
   choiceBox.classList.toggle("highlight2");
   purchase();
 }
 
 function highlight3(choiceBox) {
-    const selecionado = document.querySelector(".highlight3");
-    if (selecionado !== null) {
-        selecionado.classList.remove("highlight3");
-    }
+  const selected = document.querySelector(".highlight3");
+  if (selected !== null) {
+    selected.classList.remove("highlight3");
+  }
   choiceBox.classList.toggle("highlight3");
   purchase();
 }
 
-function purchase(){
-    const button = document.querySelector(".request-button")
-    const item1 = document.querySelector(".highlight");
-    const item2 = document.querySelector(".highlight2");
-    const item3 = document.querySelector(".highlight3");
-    if (item1 && item2 && item3 !== null) {
-        button.classList.add("request-button-highlight")
-        button.innerHTML = "<p>Fechar Pedido</p>"
-    }
-    const name1 = item1.querySelector(".food-title").textContent
-    console.log(name1)
-    const name2 = item2.querySelector(".food-title").textContent
-    console.log(name2)
-    const name3 = item3.querySelector(".food-title").textContent
-    console.log(name3)
+function purchase() {
+  const button = document.querySelector(".request-button");
+  const item1 = document.querySelector(".highlight");
+  const item2 = document.querySelector(".highlight2");
+  const item3 = document.querySelector(".highlight3");
+  if (item1 && item2 && item3 !== null) {
+    button.classList.add("request-button-highlight");
+    button.innerHTML = "<p>Fechar Pedido</p>";
+    button.onclick = function () {
+      total();
+    };
+  }
+}
+
+function total() {
+  const item1 = document.querySelector(".highlight");
+  const item2 = document.querySelector(".highlight2");
+  const item3 = document.querySelector(".highlight3");
+  const food = item1.querySelector(".food-title").textContent;
+  const drink = item2.querySelector(".food-title").textContent;
+  const dessert = item3.querySelector(".food-title").textContent;
+  let priceFood = item1.querySelector(".food-price").textContent;
+  let priceDrink = item2.querySelector(".food-price").textContent;
+  let priceDessert = item3.querySelector(".food-price").textContent;
+  priceFood = priceFood.replace("R$ ", "");
+  priceDrink = priceDrink.replace("R$ ", "");
+  priceDessert = priceDessert.replace("R$ ", "");
+  typeof(priceFood);
+  console.log(priceFood);
+  console.log(priceDrink);
+  console.log(priceDessert);
+  const sum = Number(priceFood)+Number(priceDrink)+Number(priceDessert);
+  console.log(sum);
+  let message = `Olá, gostaria de fazer o pedido:
+  - Prato: ${food}
+  - Bebida: ${drink}
+  - Sobremesa: ${dessert}
+  Total: R$ ${sum}`;
+  const encode = encodeURIComponent(message);
+  
+  //https://wa.me/1519981652372?text=urldamensagempronta
 }
