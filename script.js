c82baf8c084ebee5e6bfb07f764ec1f8ae89a342
Giovanni Marcatto <gzmarcatto@gmail.com>
2022-06-13 16:30:20 -0300

    highlights[on] whatsapp[off]

--- script.js
+++ script.js
@@ -5,29 +5,24 @@ function highlight(choiceBox) {
         selecionado.classList.remove("highlight");
     }
   choiceBox.classList.toggle("highlight");
-  console.log("highlight toggle funcionou!");
   purchase();
 }
 
 function highlight2(choiceBox) {
     const selecionado = document.querySelector(".highlight2");
-    console.log(selecionado);
     if (selecionado !== null) {
         selecionado.classList.remove("highlight2");
     }
   choiceBox.classList.toggle("highlight2");
-  console.log("highlight toggle funcionou!");
   purchase();
 }
 
 function highlight3(choiceBox) {
     const selecionado = document.querySelector(".highlight3");
-    console.log(selecionado);
     if (selecionado !== null) {
         selecionado.classList.remove("highlight3");
     }
   choiceBox.classList.toggle("highlight3");
-  console.log("highlight toggle funcionou!");
   purchase();
 }
 
@@ -40,4 +35,10 @@ function purchase(){
         button.classList.add("request-button-highlight")
         button.innerHTML = "<p>Fechar Pedido</p>"
     }
+    const name1 = item1.querySelector(".food-title").textContent
+    console.log(name1)
+    const name2 = item2.querySelector(".food-title").textContent
+    console.log(name2)
+    const name3 = item3.querySelector(".food-title").textContent
+    console.log(name3)
 }
